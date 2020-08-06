7dd6711e2ae319ccbd061dee45212722f81f717d
Y-Mona <2812211370@qq.com>
2020-08-06 12:47:48 +0800

    Y：修改了service错误重装了node modules，安装了ele-ui，主页增添了login登录入口，添加了login页面

---
+++ src/views/User/Login.vue
@@ -0,0 +1,161 @@
+<template>
+  <div class="login">
+    <el-row :gutter="20">
+      <el-col :span="14" :offset="5">
+        <el-card class="box-card">
+          <el-col :span="24" class="img_log">
+            <el-col :span="12">
+              <img alt="Vue logo" src="../../assets/logo.png" />
+            </el-col>
+
+            <el-col :span="12">
+              <el-form
+                :model="ruleForm"
+                status-icon
+                :rules="rules"
+                ref="ruleForm"
+                label-width="100px"
+                class="demo-ruleForm"
+              >
+                <el-form-item label="" prop="username">
+                  <el-input
+                    type="text"
+                    v-model="ruleForm.username"
+                    autocomplete="off"
+                    prefix-icon="el-icon-user-solid"
+                    placeholder="用户名"
+                  ></el-input>
+                </el-form-item>
+
+                <el-form-item label="" prop="pass">
+                  <el-input
+                    type="password"
+                    v-model="ruleForm.pass"
+                    autocomplete="off"
+                    prefix-icon="el-icon-lock"
+                    placeholder="密码"
+                    show-password
+                  ></el-input>
+                </el-form-item>
+
+             
+                <el-form-item>
+                  <el-button type="primary" @click="submitForm('ruleForm')" round>登录</el-button>                
+                </el-form-item>
+                <el-form-item>
+                  <el-link href="#" >还没有账号？立即注册<i class="el-icon-right"></i></el-link>             
+                </el-form-item>
+
+              </el-form>
+            </el-col>
+          </el-col>
+        </el-card>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+
+<style scoped>
+.login {
+  background-color: rgb(187, 159, 214);
+  height: 30em;
+}
+/* 布局 */
+.el-row {
+  margin-bottom: 20px;
+}
+.el-col {
+  border-radius: 4px;
+}
+/* 输入和按钮 */
+.el-input {
+  border-radius: 30px;
+}
+.el-button{
+    width: 20em;
+}
+/* 卡片 */
+.text {
+  font-size: 14px;
+}
+
+.item {
+  padding: 18px 0;
+}
+
+.box-card {
+  width: 100%;
+  height: 20em;
+  position: relative;
+  margin-top: 5em;
+  border-radius: 30px;
+}
+.img_log {
+  position: relative;
+  margin-top: 5em;
+}
+</style>
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "Login"
+};
+</script>
+
+<script>
+export default {
+    
+  data() {
+    var validatePass = (rule, value, callback) => {
+      if (value === "") {
+        callback(new Error("请输入密码"));
+      } else {
+        // if (this.ruleForm.checkPass !== "") {
+        //   this.$refs.ruleForm.validateField("checkPass");
+        // }
+        callback();
+      }
+    };
+    var validateUsername = (rule, value, callback) => {
+      if (value === "") {
+        callback(new Error("请输入用户名"));
+      } else {
+        // if (this.ruleForm.checkPass !== "") {
+        //   this.$refs.ruleForm.validateField("checkPass");
+        // }
+        callback();
+      }
+    };
+
+    return {
+      ruleForm: {
+        pass: "",
+        username: "",
+       
+      },
+      rules: {
+        pass: [{ validator: validatePass, trigger: "blur" }],
+        username: [{ validator: validateUsername, trigger: "blur" }],
+    
+      }
+    };
+  },
+  methods: {
+    submitForm(formName) {
+      this.$refs[formName].validate(valid => {
+        if (valid) {
+          alert("submit!");
+        } else {
+          console.log("error submit!!");
+          return false;
+        }
+      });
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields();
+    }
+  }
+};
+</script>
