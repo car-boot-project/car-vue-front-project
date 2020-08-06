e77942844c30f5c1ee19ce5fcd1314a3fcd021ac
Wyf33 <2959854770@qq.com>
2020-08-06 20:28:58 +0800

    王云飞-用户主页修改

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
-import Login from '../views/User/Login.vue'
 
+import Login from '../views/User/Login.vue'
+import Home from '../views/User/Home.vue'
 
 Vue.use(VueRouter)
 
   const routes = [
   {
-    path: '/',
+    path: '/home',
     name: 'Home',
     component: Home
   },
