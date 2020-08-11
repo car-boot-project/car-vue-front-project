5a5d8ab7dba495477cb24170bba32ea6c3671bf9
Y-Mona <2812211370@qq.com>
2020-08-11 15:10:42 +0800

    Y添加的session缓存，优化首页登录

--- src/views/User/Login.vue
+++ src/views/User/Login.vue
@@ -168,6 +168,8 @@ export default {
             .then(res => {
               // _this.console.log(res.data);
               if (res.data.status == 0) {
+                this.$setSessionStorage("user",res.data.obj);
+                // sessionStorage.setItem("user",res.data.obj);
                 this.$message({
                   message: "登陆成功！",
                   type: "success"
